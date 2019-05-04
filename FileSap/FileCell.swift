42f2e230bdff0c8b27a09bd58055872fca31801a
Rayquan <rhadjni@hotmail.com>
2019-05-04 16:06:46 +0800

    First Version

---
+++ FileSap/FileCell.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  FileExplorer
+//
+//  Created by GIC_UWI on 2019/5/1.
+//  Copyright © 2019 GIC. All rights reserved.
+//
+
+import UIKit
+
+class FileCell: UITableViewCell {
+
+    //@IBOutlet weak var parentnamelabel: UILabel!
+    //@IBOutlet weak var imgView: UIImageView!
+    
+    @IBOutlet weak var filenamelabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+
+}
